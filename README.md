85bfc24bf2e5b7519b842d77211eda3de9d39cc1
matthewng1996 <matthewngweile@hotmail.com>
2019-09-12 22:02:42 +0800

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# space_invaders_x86
+# Space Invaders in x86
+
+Tested on DOSBox
+
+##Steps to install, with NASM in DOSBox bin folder
+* Download/Clone file, place asm file in DOSBox bin folder.
+* Using DOSBox, mount directory which stores this asm file
+* Type the following command: nasm spaceinvaders.asm -o spaceinvaders.exe
+* Type the following command: spaceinvaders.exe
+* ENJOY!
---
+++ space_invaders.asm
@@ -0,0 +1,15 @@
+; Space Invaders game written in x86 assembly language (32-bit)
+; by Matthew Ng, 2019 -> Day 0 on 9TH SEPTEMBER 2019
+; http://mattcomputingjourney.wordpress.com/
+;
+; Tested on DOSBox
+; I have developed games (Mainly Android) for the past 3 years as a past time, mainly in C#. Now i would like
+; to explore creating games using assembly language! I bet it's gonna be fun!
+;
+;
+;
+;
+;
+; Would like to see how long i would need to finish this side project and hopefully by the end of it
+; widen my understanding of assembly language! :)
+
